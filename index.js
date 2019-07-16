2c8e4c8d65d60ce3d3f51d56c46429306d78e1cb
Charles S <charles.selvarajan@gmail.com>
2019-07-16 10:33:14 -0700

    [minor] support custom path lib to handle custom fs (#1)

--- index.js
+++ index.js
@@ -20,16 +20,12 @@ var accepts = require('accepts');
 var createError = require('http-errors');
 var debug = require('debug')('serve-index');
 var escapeHtml = require('escape-html');
-var fs = require('fs')
-  , path = require('path')
-  , normalize = path.normalize
-  , sep = path.sep
-  , extname = path.extname
-  , join = path.join;
+var fs = require('fs');
 var Batch = require('batch');
 var mime = require('mime-types');
 var parseUrl = require('parseurl');
-var resolve = require('path').resolve;
+var nativePathLib = require('path');
+
 
 /**
  * Module exports.
@@ -48,13 +44,13 @@ var cache = {};
  * Default template.
  */
 
-var defaultTemplate = join(__dirname, 'public', 'directory.html');
+var defaultTemplate = nativePathLib.join(__dirname, 'public', 'directory.html');
 
 /*!
  * Stylesheet.
  */
 
-var defaultStylesheet = join(__dirname, 'public', 'style.css');
+var defaultStylesheet = nativePathLib.join(__dirname, 'public', 'style.css');
 
 /**
  * Media types and the map for content negotiation.
@@ -91,100 +87,106 @@ function serveIndex(root, options) {
     throw new TypeError('serveIndex() root path required');
   }
 
+  // mainly for Windows to use posix() for MemFS
+  this.pathLib = opts.path || nativePathLib;
   // resolve root to absolute and normalize
-  var rootPath = normalize(resolve(root) + sep);
+  this.rootPath = this.pathLib.normalize(this.pathLib.resolve(root) + this.pathLib.sep);
+  // retreive other options
+  this.filter = opts.filter;
+  this.hidden = opts.hidden;
+  this.showIcons = opts.icons;
+  this.stylesheet = opts.stylesheet || defaultStylesheet;
+  this.template = opts.template || defaultTemplate;
+  this.view = opts.view || 'tiles';
+  this.filesystem = opts.fs || fs;
+}
 
-  var filter = opts.filter;
-  var hidden = opts.hidden;
-  var icons = opts.icons;
-  var stylesheet = opts.stylesheet || defaultStylesheet;
-  var template = opts.template || defaultTemplate;
-  var view = opts.view || 'tiles';
-  var filesystem = opts.fs || fs;
+serveIndex.prototype.indexServer = function(req, res, next) {
+  if (req.method !== 'GET' && req.method !== 'HEAD') {
+    res.statusCode = 'OPTIONS' === req.method ? 200 : 405;
+    res.setHeader('Allow', 'GET, HEAD, OPTIONS');
+    res.setHeader('Content-Length', '0');
+    res.end();
+    return;
+  }
 
-  return function (req, res, next) {
-    if (req.method !== 'GET' && req.method !== 'HEAD') {
-      res.statusCode = 'OPTIONS' === req.method ? 200 : 405;
-      res.setHeader('Allow', 'GET, HEAD, OPTIONS');
-      res.setHeader('Content-Length', '0');
-      res.end();
-      return;
+  // parse URLs
+  var url = parseUrl(req);
+  var originalUrl = parseUrl.original(req);
+  var dir = decodeURIComponent(url.pathname);
+  var originalDir = decodeURIComponent(originalUrl.pathname);
+
+  // join / normalize from root dir
+  var path = this.pathLib.normalize(this.pathLib.join(this.rootPath, dir));
+
+  // null byte(s), bad request
+  if (~path.indexOf('\0')) return next(createError(400));
+
+  // malicious path
+  if ((path + this.pathLib.sep).substr(0, this.rootPath.length) !== this.rootPath) {
+    debug('malicious path "%s"', path);
+    return next(createError(403));
+  }
+
+  // determine ".." display
+  var showUp = this.pathLib.normalize(this.pathLib.resolve(path) + this.pathLib.sep) !== this.rootPath;
+
+  // check if we have a directory
+  debug('stat "%s"', path);
+  var _self = this;
+  this.filesystem.stat(path, function(err, stat){
+    if (err && err.code === 'ENOENT') {
+      return next();
     }
 
-    // parse URLs
-    var url = parseUrl(req);
-    var originalUrl = parseUrl.original(req);
-    var dir = decodeURIComponent(url.pathname);
-    var originalDir = decodeURIComponent(originalUrl.pathname);
-
-    // join / normalize from root dir
-    var path = normalize(join(rootPath, dir));
-
-    // null byte(s), bad request
-    if (~path.indexOf('\0')) return next(createError(400));
-
-    // malicious path
-    if ((path + sep).substr(0, rootPath.length) !== rootPath) {
-      debug('malicious path "%s"', path);
-      return next(createError(403));
+    if (err) {
+      err.status = err.code === 'ENAMETOOLONG'
+        ? 414
+        : 500;
+      return next(err);
     }
 
-    // determine ".." display
-    var showUp = normalize(resolve(path) + sep) !== rootPath;
+    if (!stat.isDirectory()) return next();
 
-    // check if we have a directory
-    debug('stat "%s"', path);
-    filesystem.stat(path, function(err, stat){
-      if (err && err.code === 'ENOENT') {
-        return next();
-      }
-
-      if (err) {
-        err.status = err.code === 'ENAMETOOLONG'
-          ? 414
-          : 500;
-        return next(err);
-      }
-
-      if (!stat.isDirectory()) return next();
-
-      // fetch files
-      debug('readdir "%s"', path);
-      filesystem.readdir(path, function(err, files){
-        if (err) return next(err);
-        if (!hidden) files = removeHidden(files);
-        if (filter) files = files.filter(function(filename, index, list) {
-          return filter(filename, index, list, path);
-        });
-        files.sort();
-
-        // content-negotiation
-        var accept = accepts(req);
-        var type = accept.type(mediaTypes);
-
-        // not acceptable
-        if (!type) return next(createError(406));
-        serveIndex[mediaType[type]](req, res, files, next, originalDir, showUp, icons, path, view, template, stylesheet, filesystem);
+    // fetch files
+    debug('readdir "%s"', path);
+    _self.filesystem.readdir(path, function(err, files){
+      if (err) return next(err);
+      if (!_self.hidden) files = _self.removeHidden(files);
+      if (_self.filter) files = files.filter(function(filename, index, list) {
+        return _self.filter(filename, index, list, path);
       });
+      files.sort();
+
+      // content-negotiation
+      var accept = accepts(req);
+      var type = accept.type(mediaTypes);
+
+      // not acceptable
+      if (!type) return next(createError(406));
+      // find the relevant media-type to send the response
+      var media = mediaType[type];
+      var handlerContext = typeof serveIndex[media] === 'function' ? undefined : _self;
+      (serveIndex[media] || _self[media]).call(handlerContext, req, res, files, next, originalDir, showUp, _self.showIcons, path, _self.view, _self.template, _self.stylesheet, _self.filesystem);
     });
-  };
-};
+  });
+}
 
 /**
  * Respond with text/html.
  */
-
-serveIndex.html = function _html(req, res, files, next, dir, showUp, icons, path, view, template, stylesheet, filesystem) {
+serveIndex.prototype.html = function(req, res, files, next, dir, showUp, displayIcons, path, view, template, stylesheet, filesystem) {
   var render = typeof template !== 'function'
-    ? createHtmlRender(template)
+    ? this.createHtmlRender(template)
     : template
 
   if (showUp) {
     files.unshift('..');
   }
 
+  var _self = this;
   // stat all files
-  statFiles(path, files, filesystem, function (err, stats) {
+  this.statFiles(path, files, filesystem, function (err, stats) {
     if (err) return next(err);
 
     // combine the stats into the file list
@@ -193,7 +195,7 @@ serveIndex.html = function _html(req, res, files, next, dir, showUp, icons, path
     });
 
     // sort file list
-    fileList.sort(fileSort);
+    fileList.sort(_self.fileSort);
 
     // read stylesheet
     fs.readFile(stylesheet, 'utf8', function (err, style) {
@@ -202,7 +204,7 @@ serveIndex.html = function _html(req, res, files, next, dir, showUp, icons, path
       // create locals for rendering
       var locals = {
         directory: dir,
-        displayIcons: Boolean(icons),
+        displayIcons: Boolean(displayIcons),
         fileList: fileList,
         path: path,
         style: style,
@@ -212,7 +214,7 @@ serveIndex.html = function _html(req, res, files, next, dir, showUp, icons, path
       // render html
       render(locals, function (err, body) {
         if (err) return next(err);
-        send(res, 'text/html', body)
+        _self.send(res, 'text/html', body)
       });
     });
   });
@@ -221,25 +223,23 @@ serveIndex.html = function _html(req, res, files, next, dir, showUp, icons, path
 /**
  * Respond with application/json.
  */
-
-serveIndex.json = function _json(req, res, files) {
-  send(res, 'application/json', JSON.stringify(files))
+serveIndex.prototype.json = function(req, res, files) {
+  this.send(res, 'application/json', JSON.stringify(files))
 };
 
 /**
  * Respond with text/plain.
  */
-
-serveIndex.plain = function _plain(req, res, files) {
-  send(res, 'text/plain', (files.join('\n') + '\n'))
+serveIndex.prototype.plain = function(req, res, files) {
+  this.send(res, 'text/plain', (files.join('\n') + '\n'))
 };
 
 /**
  * Map html `files`, returning an html unordered list.
  * @private
  */
-
-function createHtmlFileList(files, dir, useIcons, view) {
+serveIndex.prototype.createHtmlFileList = function(files, dir, useIcons, view) {
+  var _self = this;
   var html = '<ul id="files" class="view-' + escapeHtml(view) + '">'
     + (view === 'details' ? (
       '<li class="header">'
@@ -259,8 +259,8 @@ function createHtmlFileList(files, dir, useIcons, view) {
       if (isDir) {
         classes.push('icon-directory');
       } else {
-        var ext = extname(file.name);
-        var icon = iconLookup(file.name);
+        var ext = _self.pathLib.extname(file.name);
+        var icon = _self.iconLookup(file.name);
 
         classes.push('icon');
         classes.push('icon-' + ext.substring(1));
@@ -281,7 +281,7 @@ function createHtmlFileList(files, dir, useIcons, view) {
       : '';
 
     return '<li><a href="'
-      + escapeHtml(normalizeSlashes(normalize(path.join('/'))))
+      + escapeHtml(_self.normalizeSlashes(_self.pathLib.normalize(path.join('/'))))
       + '" class="' + escapeHtml(classes.join(' ')) + '"'
       + ' title="' + escapeHtml(file.name) + '">'
       + '<span class="name">' + escapeHtml(file.name) + '</span>'
@@ -298,18 +298,18 @@ function createHtmlFileList(files, dir, useIcons, view) {
 /**
  * Create function to render html.
  */
-
-function createHtmlRender(template) {
+serveIndex.prototype.createHtmlRender = function(template) {
+  var _self = this;
   return function render(locals, callback) {
     // read template
     fs.readFile(template, 'utf8', function (err, str) {
       if (err) return callback(err);
 
       var body = str
-        .replace(/\{style\}/g, locals.style.concat(iconStyle(locals.fileList, locals.displayIcons)))
-        .replace(/\{files\}/g, createHtmlFileList(locals.fileList, locals.directory, locals.displayIcons, locals.viewName))
+        .replace(/\{style\}/g, locals.style.concat(_self.iconStyle(locals.fileList, locals.displayIcons)))
+        .replace(/\{files\}/g, _self.createHtmlFileList(locals.fileList, locals.directory, locals.displayIcons, locals.viewName))
         .replace(/\{directory\}/g, escapeHtml(locals.directory))
-        .replace(/\{linked-path\}/g, htmlPath(locals.directory));
+        .replace(/\{linked-path\}/g, _self.htmlPath(locals.directory));
 
       callback(null, body);
     });
@@ -319,8 +319,7 @@ function createHtmlRender(template) {
 /**
  * Sort function for with directories first.
  */
-
-function fileSort(a, b) {
+serveIndex.prototype.fileSort = function(a, b) {
   // sort ".." to the top
   if (a.name === '..' || b.name === '..') {
     return a.name === b.name ? 0
@@ -334,8 +333,7 @@ function fileSort(a, b) {
 /**
  * Map html `dir`, returning a linked path.
  */
-
-function htmlPath(dir) {
+serveIndex.prototype.htmlPath = function(dir) {
   var parts = dir.split('/');
   var crumb = new Array(parts.length);
 
@@ -354,9 +352,8 @@ function htmlPath(dir) {
 /**
  * Get the icon data for the file name.
  */
-
-function iconLookup(filename) {
-  var ext = extname(filename);
+serveIndex.prototype.iconLookup = function(filename) {
+  var ext = this.pathLib.extname(filename);
 
   // try by extension
   if (icons[ext]) {
@@ -412,8 +409,7 @@ function iconLookup(filename) {
 /**
  * Load icon images, return css string.
  */
-
-function iconStyle(files, useIcons) {
+serveIndex.prototype.iconStyle = function(files, useIcons) {
   if (!useIcons) return '';
   var i;
   var list = [];
@@ -428,13 +424,13 @@ function iconStyle(files, useIcons) {
     var isDir = file.stat && file.stat.isDirectory();
     var icon = isDir
       ? { className: 'icon-directory', fileName: icons.folder }
-      : iconLookup(file.name);
+      : this.iconLookup(file.name);
     var iconName = icon.fileName;
 
     selector = '#files .' + icon.className + ' .name';
 
     if (!rules[iconName]) {
-      rules[iconName] = 'background-image: url(data:image/png;base64,' + load(iconName) + ');'
+      rules[iconName] = 'background-image: url(data:image/png;base64,' + this.load(iconName) + ');'
       selectors[iconName] = [];
       list.push(iconName);
     }
@@ -459,8 +455,7 @@ function iconStyle(files, useIcons) {
  * @return {String}
  * @api private
  */
-
-function load(icon) {
+serveIndex.prototype.load = function(icon) {
   if (cache[icon]) return cache[icon];
   return cache[icon] = fs.readFileSync(__dirname + '/public/icons/' + icon, 'base64');
 }
@@ -473,9 +468,8 @@ function load(icon) {
  * @return {String}
  * @api private
  */
-
-function normalizeSlashes(path) {
-  return path.split(sep).join('/');
+serveIndex.prototype.normalizeSlashes = function(path) {
+  return path.split(this.pathLib.sep).join('/');
 };
 
 /**
@@ -486,8 +480,7 @@ function normalizeSlashes(path) {
  * @return {Array}
  * @api private
  */
-
-function removeHidden(files) {
+serveIndex.prototype.removeHidden = function(files) {
   return files.filter(function(file){
     return file[0] !== '.'
   });
@@ -497,8 +490,7 @@ function removeHidden(files) {
  * Send a response.
  * @private
  */
-
-function send (res, type, body) {
+serveIndex.prototype.send = function(res, type, body) {
   // security header for content sniffing
   res.setHeader('X-Content-Type-Options', 'nosniff')
 
@@ -514,15 +506,14 @@ function send (res, type, body) {
  * Stat all files and return array of stat
  * in same order.
  */
-
-function statFiles(dir, files, filesystem, cb) {
+serveIndex.prototype.statFiles = function(dir, files, filesystem, cb) {
   var batch = new Batch();
-
   batch.concurrency(10);
 
+  var _self = this;
   files.forEach(function(file){
     batch.push(function(done){
-      filesystem.stat(join(dir, file), function(err, stat){
+      filesystem.stat(_self.pathLib.join(dir, file), function(err, stat){
         if (err && err.code !== 'ENOENT') return done(err);
 
         // pass ENOENT as null stat, not error
@@ -534,10 +525,15 @@ function statFiles(dir, files, filesystem, cb) {
   batch.end(cb);
 }
 
+// custom response handlers
+//  to be overridden when needed
+serveIndex.html = null;
+serveIndex.plain = null;
+serveIndex.json = null;
+
 /**
  * Icon map.
  */
-
 var icons = {
   // base icons
   'default': 'page_white.png',
